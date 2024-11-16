93cf241c4f314a0299d01f627a82f5335628a74c
Surender Pratap Singh <rk31151@gmail.com>
2024-11-16 18:03:18 +0530

    create login and register form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import RegisterForm from "./RegisterForm";
 
 const page = () => {
-    return <div>page</div>;
+    return (
+        <div className="flex items-center justify-center vertical-center">
+            <RegisterForm />
+        </div>
+    );
 };
 
 export default page;
